25df0c20cdbbbe75833e1c0dd73626d3a30f56b1
konstasuomalainen <122342899+konstasuomalainen@users.noreply.github.com>
2024-02-15 15:59:02 +0200

    Update aika.py

--- aika.py
+++ aika.py
@@ -2,13 +2,13 @@ import time
 from datetime import date
 from datetime import datetime
 def aika():
-    #PÄIVÄMÄÄRÄ TÄNÄÄN
+    #DATE, but it has year inside it
     today = date.today()
-
+    #Need to split day and month from today
     datetime_month = today.month
     datetime_day = today.day
-    #YHDISTETÄÄN PÄIVÄ JA KUUKAUSI
-    aika_tänään = (f'{datetime_day}.{datetime_month}')
+    #day + month
+    day_today = (f'{datetime_day}.{datetime_month}')
     #MUUTETAAN STRING MUOTOON
-    inputText = aika_tänään
-    return inputText
+    
+    return day_today
