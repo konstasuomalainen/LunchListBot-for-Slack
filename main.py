1602ce983f20844c54f7da39e55f8918296e5b2e
konstasuomalainen <122342899+konstasuomalainen@users.noreply.github.com>
2024-02-22 11:59:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,13 @@ import slack_sdk
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
-from fpdf import FPDF
-import requests 
-
+import requests
 from aika import *
 from ruokapaikat import *
 from blockkit import Button, Context, Divider, Message, Section
-
-import json
-
 from slack_sdk.webhook import WebhookClient
-
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
-
 import time
 
 env_path = Path('.') /'.env'
@@ -41,14 +32,21 @@ voter_ids3 = []
 voter_ids4 = []
 voter_ids5 = []
 voter_ids6 = []
+voter_ids7 = []
+voter_ids8 = []
+voter_idsMuu = []
 vote_count1= []
 vote_count2= []
 vote_count3= []
 vote_count4= []
 vote_count5= []
 vote_count6= []
+vote_count7= []
+vote_count8= []
+vote_countMuu= []
 
-all_restaurants = ['Cafe Hullu','Pancho Villa','Wolkoff', 'Wilhelmiina','Elsi','Kehruuhuone']
+
+all_restaurants = ['Cafe Hullu','Pancho Villa','Wolkoff', 'Wilhelmiina','Elsi','Kehruuhuone','Vapari', 'Kitchen', 'Muu']
 
 response = app.client.conversations_list()
 channel_name = 'lunch-bot'  # Replace with your channel name
@@ -69,26 +67,34 @@ menu3 = find_ruoka3()
 menu4 = find_ruoka4()
 menu5 = find_ruoka5()
 menu6 = find_ruoka6()
+menu7 = find_ruoka7()
+menu8 = find_ruoka8()
 
 
 
     
 def ruokanaTänään():
     messages_sent = False
-    
 
-    client.chat_postMessage(channel='#lunch-bot', text="",
-            blocks=[
-            {
-			"type": "header",
-			"text": {
+    # List of menus
+    menus = [menu1, menu2, menu3, menu4, menu5, menu6, menu7, menu8]
+
+    for menu in menus:
+        for ruokana in menu:
+            ruokana = ruokana.text
+
+            # If today's date is found on the menu, the menu will be printed to Slack
+            if aika() in ruokana:
+                client.chat_postMessage(channel='#lunch-bot', text="",
+                blocks=[
+                {
+			    "type": "header",
+			    "text": {
 				"type": "plain_text",
 				"text": ":newspaper:  Päivän Lounaslista  :newspaper:"
-			},
-                
-            }
-            ])
-
+			    },}])
+                break
+        break
 
 
 
@@ -266,9 +272,84 @@ def ruokanaTänään():
         
         else:
             #Weekend or no meals
-            print("ei ruokailua")       
-    
-    
+            print("ei ruokailua")      
+
+    for ruokana in menu7:
+        ruokana = ruokana.text
+        #If today's date is found on the menu, the menu will be printed to Slack
+        if aika() in ruokana:
+            
+            ruokana = ruokana.replace('€', ' euro ')
+            
+            
+            res = [all_restaurants[6]]
+            result = ''.join(res)
+            print(ruokana)
+            client.chat_postMessage(channel='#lunch-bot', text="",
+            blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f'*{result}*:\n\n{ruokana}!'},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Vote"},
+                    "action_id": "vote_click7"
+                },
+                
+            }
+            ])
+            messages_sent = True
+        
+        
+        else:
+            #Weekend or no meals
+            print("ei ruokailua")  
+
+    for ruokana in menu8:
+        ruokana = ruokana.text
+        #If today's date is found on the menu, the menu will be printed to Slack
+        if aika() in ruokana:
+            
+            ruokana = ruokana.replace('€', ' euro ')
+            
+            
+            res = [all_restaurants[7]]
+            result = ''.join(res)
+            print(ruokana)
+            client.chat_postMessage(channel='#lunch-bot', text="",
+            blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f'*{result}*:\n\n{ruokana}!'},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Vote"},
+                    "action_id": "vote_click8"
+                },
+                
+            }
+            ])
+            messages_sent = True
+        
+        
+        else:
+            #Weekend or no meals
+            print("ei ruokailua")   
+    if messages_sent == True:
+        client.chat_postMessage(channel='#lunch-bot', text="",
+            blocks=[
+            {
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f'*Muu ravintola*'},
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Vote"},
+                    "action_id": "vote_click9"
+                },
+                
+            }
+            ])
+            
     if messages_sent == True:
         global VoteMessageTs
         vastaus1=client.chat_postMessage(channel='#lunch-bot', text="*Kukaan ei ole äänestänyt vielä*")
@@ -290,6 +371,9 @@ def äänestys():
     global vote_count4
     global vote_count5
     global vote_count6
+    global vote_count7
+    global vote_count8
+    global vote_countMuu
     global VoteMessageTs
     vote_count1 = len(voter_ids1)
     vote_count2 = len(voter_ids2)
@@ -297,6 +381,9 @@ def äänestys():
     vote_count4 = len(voter_ids4)
     vote_count5 = len(voter_ids5)
     vote_count6 = len(voter_ids6)
+    vote_count7 = len(voter_ids7)
+    vote_count8 = len(voter_ids8)
+    vote_countMuu = len(voter_idsMuu)
 
     if VoteMessageTs:
             
@@ -313,6 +400,13 @@ def äänestys():
             res5 = ''.join(res5)
             res6 = [all_restaurants[5]]
             res6 = ''.join(res6)
+            res7 = [all_restaurants[6]]
+            res7 = ''.join(res7)
+            res8 = [all_restaurants[7]]
+            res8 = ''.join(res8)
+            resMuu = [all_restaurants[8]]
+            resMuu = ''.join(resMuu)
+            
  
 
             # Create a list of tuples containing vote counts, restaurant names, and indices
@@ -322,7 +416,10 @@ def äänestys():
             (vote_count3, res3, 3),
             (vote_count4, res4, 4),
             (vote_count5, res5, 5),
-            (vote_count6, res6, 6)
+            (vote_count6, res6, 6),
+            (vote_count7, res7, 7),
+            (vote_count8, res6, 8),
+            (vote_countMuu, resMuu, 9)
             ]
 
             # Sort the list based on vote counts in descending order
@@ -421,6 +518,42 @@ def action_button_click3(body, ack):
         voter_ids6.append(user_id)
         
     äänestys()
+@app.action("vote_click7")
+def action_button_click3(body, ack):
+    
+    ack()
+    user_id = body['user']['id']
+    if user_id in voter_ids7:
+        voter_ids7.remove(user_id)
+    else:
+        voter_ids7.append(user_id)
+        
+    äänestys()
+
+@app.action("vote_click8")
+def action_button_click3(body, ack):
+    
+    ack()
+    user_id = body['user']['id']
+    if user_id in voter_ids8:
+        voter_ids8.remove(user_id)
+    else:
+        voter_ids8.append(user_id)
+        
+    äänestys()
+
+@app.action("vote_click9")
+def action_button_click3(body, ack):
+    
+    ack()
+    user_id = body['user']['id']
+    if user_id in voter_idsMuu:
+        voter_idsMuu.remove(user_id)
+    else:
+        voter_idsMuu.append(user_id)
+        
+    äänestys()
+
 
 
 
